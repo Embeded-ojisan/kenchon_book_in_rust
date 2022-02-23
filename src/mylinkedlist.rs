6cac1573515e5135086445ecd1da85ff1325eac3
lowlevelengineer@gmail.com <lowlevelengineer@gmail.com>
2022-02-23 20:53:24 +0900

    commit

--- src/mylinkedlist.rs
+++ src/mylinkedlist.rs
@@ -1,5 +1,7 @@
+use crate::mylinkedlist::MyLinkedList::Nil;
+
 pub enum MyLinkedList<T>{
-    Some(Node<T>),
+    Some(Box<Node<T>>),
     Nil,
 }
 
@@ -8,10 +10,48 @@ pub struct Node<T> {
     data: T,
 }
 
-/*
-impl MyLinkedList<T> {
-    fn new() -> MyLinkedList {
+
+impl<T> MyLinkedList<T> {
+    pub fn new() -> MyLinkedList<T> {
         Nil
     }
-}
+
+/*
+    fn len(&self) -> usize {
+
+    }
 */
+
+    pub fn insert(&mut self, data: T) {
+        match self {
+            Nil => {
+
+            },
+            _ => {
+/*
+                let old_node = self.unwrap();
+                let new_node = Box::new(
+                    Node {
+                        next: old_node.next,
+                        data: data,
+                    }
+                );
+
+                s.next = new_node;
+*/
+            },
+        }
+    }
+
+/*
+    fn erase(&mut self, data: T) -> {
+
+    }
+*/
+
+/*
+    fn print(&mut self) -> {
+
+    }
+*/
+}
