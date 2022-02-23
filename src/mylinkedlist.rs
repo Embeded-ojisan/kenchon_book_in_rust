bde4cd3ef7ed3bba20b58d41b7dc0e463924748e
lowlevelengineer@gmail.com <lowlevelengineer@gmail.com>
2022-02-23 23:58:22 +0900

    commit

--- src/mylinkedlist.rs
+++ src/mylinkedlist.rs
@@ -1,26 +1,57 @@
-use crate::mylinkedlist::MyLinkedList::Nil;
+/*
+ベースは以下
 
-pub enum MyLinkedList<T>{
-    Some(Box<Node<T>>),
-    Nil,
+https://rust-unofficial.github.io/too-many-lists/second-final.html
+
+http://www.nct9.ne.jp/m_hiroi/linux/rust04.html
+*/
+
+pub struct MyLinkedList<T> {
+    head: Link<T>,
 }
 
+type Link<T> = Option<Box<Node<T>>>;
+
 pub struct Node<T> {
-    next: Box<Node<T>>,
-    data: T,
+    elem: T,
+    next: Link<T>,
 }
 
 
 impl<T> MyLinkedList<T> {
-    pub fn new() -> MyLinkedList<T> {
-        Nil
+
+    pub fn new() -> Self {
+        MyLinkedList {
+            head: None,
+        }
     }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(
+            Node {
+                elem: elem,
+                next: self.head.take()
+            }
+        );
+
+        self.head = Some(new_node);
+    }
+
+/*
+    pub fn pop(&mut self) -> Link<T> {
+        let node = self.head;
+        self.head = self.head.take().next;
+        node
+    }
+*/
 
 /*
     fn len(&self) -> usize {
 
     }
 */
+    
+/*
 
     pub fn insert(&mut self, data: T) {
         match self {
@@ -28,7 +59,6 @@ impl<T> MyLinkedList<T> {
 
             },
             _ => {
-/*
                 let old_node = self.unwrap();
                 let new_node = Box::new(
                     Node {
@@ -38,11 +68,11 @@ impl<T> MyLinkedList<T> {
                 );
 
                 s.next = new_node;
-*/
             },
         }
     }
-
+*/
+    
 /*
     fn erase(&mut self, data: T) -> {
 
