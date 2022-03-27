8dd0571eb3b846c442014b0d8397220832d18606
Local PC <lowlevelengineer@gmail.com>
2022-03-27 17:44:00 +0900

    commit

--- src/UnionFind.rs
+++ src/UnionFind.rs
@@ -1,19 +1,29 @@
 
 
-struct UnionFind{
+struct UnionFind
+{
     par: Vec<Option<usize>>,
     siz: Vec<usize>,
 }
 
-impl UnionFind{
-    pub fn new(n: usize) -> Self{
-        UnionFind {
+pub enum UniteError
+{
+    AlreadySameGroup,
+}
+
+impl UnionFind
+{
+    pub fn new(n: usize) -> Self
+    {
+        UnionFind
+        {
             par: vec![None; n],
             siz: vec![1; n],
         }
     }
 
-    pub fn root(&mut self, x: usize) -> Option<usize> {
+    pub fn root(&mut self, x: usize) -> Option<usize>
+    {
         if self.par[0] == None 
         {
             return Some(x);
@@ -23,5 +33,24 @@ impl UnionFind{
             self.par[x] = self.root(self.par[x].unwrap());
             return self.par[x];
         }
+    }
+
+    pub fn issame(&mut self, x: usize, y: usize) -> bool
+    {
+        // Option型がEqトレートに対応しているため
+        return self.root(x) == self.root(y);     
+    }
+
+    pub fn unite(&mut self, x: usize, y: usize) -> Result<,UniteError>
+    {
+        let x = self.root(x);
+        let y = self.root(y);
+
+        if x == y
+        {
+            return Error(UnionError);
+        }
+
+        return Ok();
     }
 }
