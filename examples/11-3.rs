7e3e7271e88bb3a7abd2de7dbd17baca63d36f9a
Local PC <lowlevelengineer@gmail.com>
2022-03-31 00:14:09 +0900

    commit

--- examples/11-3.rs
+++ examples/11-3.rs
@@ -10,9 +10,18 @@ fn main()
     uf.unite(2,3);
     uf.unite(5,6);
 
-    println!("uf.issame(1,3) is {}", uf.issame(1,3));
-    println!("uf.issame(2,5) is {}", uf.issame(2,5));
+    println!(
+        "uf.issame(1,3) is {}",
+        uf.issame(1,3)
+    );
+    println!(
+        "uf.issame(2,5) is {}",
+        uf.issame(2,5)
+    );
 
     uf.unite(1,6);
-    println!("uf.issame(2,5) is {}", uf.issame(2,5));
+    println!(
+        "uf.issame(2,5) is {}",
+        uf.issame(2,5)
+    );
 }
