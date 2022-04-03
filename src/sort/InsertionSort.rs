3dd5b0119cca273b5e2bd3d481e29c335e5e1db0
Local PC <lowlevelengineer@gmail.com>
2022-04-04 00:01:06 +0900

    commit

---
+++ src/sort/InsertionSort.rs
@@ -0,0 +1,28 @@
+
+pub fn InsertionSort(a: &mut Vec<usize>)
+{
+    let mut N = a.len();
+    for (i, val) in a.iter_mut().enumerate()
+    {
+        let v = val;
+        let j = i;
+        loop
+        {
+            if j <= 0
+            {
+                break;
+            }
+
+            if a[j - 1] > *val
+            {
+                let aj = a[j - 1];
+                a[j] = aj;
+            }
+            else
+            {
+                break;
+            }
+        }
+        a[j] = *v;
+    }
+}
