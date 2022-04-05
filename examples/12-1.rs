1af2c452140a07977ccd26ce1c069f7304b49623
Local PC <lowlevelengineer@gmail.com>
2022-04-05 23:33:04 +0900

    commit

--- examples/12-1.rs
+++ examples/12-1.rs
@@ -1,6 +1,17 @@
 use kenchon_book_in_rust::sort::InsertionSort;
+use kenchon_book_in_rust::utility::stdio_wrapper;
 
 pub fn main()
 {
-    ;
+    let v = Vec::new();
+ 
+    let N = read_single_usize();
+
+    for i in n
+    {
+        let a = read_single_usize();
+        v.push(a);
+    }
+
+    InsertionSort(a);
 }
